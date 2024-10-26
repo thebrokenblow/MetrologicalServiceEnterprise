7caab74bf672781505dbd8721809663257dcf169
Krasov <artkrasov@gmail.com>
2024-10-26 23:21:43 +0300

    Добавьте файлы проекта.

---
+++ MetrologicalServiceEnterprise.View/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace MetrologicalServiceEnterprise.View;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
